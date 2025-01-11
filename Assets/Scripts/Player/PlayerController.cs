8f3d887b34ebc2ca197585596a4ed9ee114582f6
paquan820@gmail.com <sieunhi164@gmail.com>
2025-01-12 00:56:19 +0700

    Thay doi script nhan vat

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,7 +6,16 @@ using UnityEngine.InputSystem;
 // Takes and handles input and movement for a player character
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 1f;
+    public bool IsMoving{
+        set{
+            isMoving = value;
+            animator.SetBool("isMoving", value);
+        }
+    }
+
+    public float moveSpeed = 700f;
+    public float maxSpeed = 2.2f;
+    public float idleFriction = 0.9f;
     public float collisionOffset = 0.05f;
     public ContactFilter2D movementFilter;
     public SwordAttack swordAttack;
@@ -19,6 +28,8 @@ public class PlayerController : MonoBehaviour
 
     bool canMove = true;
 
+    private bool isMoving = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,54 +39,22 @@ public class PlayerController : MonoBehaviour
     }
 
     private void FixedUpdate() {
-        if(canMove) {
+        if(canMove == true && movementInput != Vector2.zero) {
             // If movement input is not 0, try to move
-            if(movementInput != Vector2.zero){
-                
-                bool success = TryMove(movementInput);
-
-                if(!success) {
-                    success = TryMove(new Vector2(movementInput.x, 0));
-                }
-
-                if(!success) {
-                    success = TryMove(new Vector2(0, movementInput.y));
-                }
-                
-                animator.SetBool("isMoving", success);
-            } else {
-                animator.SetBool("isMoving", false);
-            }
-
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity + (movementInput * moveSpeed * Time.deltaTime), maxSpeed);
             // Set direction of sprite to movement direction
             if(movementInput.x < 0) {
                 spriteRenderer.flipX = true;
             } else if (movementInput.x > 0) {
                 spriteRenderer.flipX = false;
             }
-        }
-    }
 
-    private bool TryMove(Vector2 direction) {
-        if(direction != Vector2.zero) {
-            // Check for potential collisions
-            int count = rb.Cast(
-                direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
-                movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
-                castCollisions, // List of collisions to store the found collisions into after the Cast is finished
-                moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
-
-            if(count == 0){
-                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            // Can't move if there's no direction to move in
-            return false;
+            IsMoving = true;
         }
-        
+        else{
+            rb.velocity = Vector2.Lerp(rb.velocity, Vector2.zero, idleFriction);
+            IsMoving = false;
+        }
     }
 
     void OnMove(InputValue movementValue) {
